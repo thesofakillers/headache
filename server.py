c746da9936b5ee2ff6c89ea89e3349c3169d4d8c
Giulio Starace <giulio.starace@gmail.com>
2021-02-07 18:49:42 +0100

    first working dash

--- server.py
+++ server.py
@@ -1,35 +1,49 @@
 """Dash server index point"""
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DAYLIO_PATH = os.getenv("DAYLIO_PATH")
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
+FIELDS = ["full_date", "Headache", "Exercise", "Ibuprofen", "Paracetamol", "Coffee"]
+daylio_df = pd.read_csv(
+    DAYLIO_PATH,
+    usecols=FIELDS,
+    index_col="full_date",
+    parse_dates=True,
+)
+daylio_df = daylio_df.resample("1D").first()
+
+
+TOTAL_FIELDS = len(FIELDS)
+
+fig = make_subplots(
+    rows=TOTAL_FIELDS - 1, cols=1, shared_xaxes=True, vertical_spacing=0.02
 )
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+for i, field in enumerate(FIELDS[1:], 1):
+    fig.add_trace(
+        go.Scatter(x=daylio_df.index, y=daylio_df[field], line_shape="hv", name=field),
+        row=i,
+        col=1,
+    )
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Hello Dash"),
-        html.Div(
-            children="""
-        Dash: A web application framework for Python.
-        """
-        ),
-        dcc.Graph(id="example-graph", figure=fig),
+        html.H1("Headache data visualization"),
+        dcc.Graph(id="daylio", figure=fig),
     ]
 )
 
