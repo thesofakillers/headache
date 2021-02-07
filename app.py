8f0617dfdde681856b74284736227bb31ddd63b9
Giulio Starace <giulio.starace@gmail.com>
2021-02-07 19:10:08 +0100

    try fix

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 FIELDS = ["full_date", "Headache", "Exercise", "Ibuprofen", "Paracetamol", "Coffee"]
 daylio_df = pd.read_csv(
     DAYLIO_PATH,
